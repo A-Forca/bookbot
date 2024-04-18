189d57b59a23e74f734651aca3864205087c99c9
Anthony Forca <freecodecamp4716@gmail.com>
2024-04-18 11:55:02 -0400

    charcount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
 
-    with open("bookbot/books/frankenstein.txt") as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
     def numOfWords():
@@ -12,9 +12,18 @@ def main():
 
     numOfWords()
 
-    def numOfChar():
-        charDict = {}
-        for char in file_contents:
+    def numOfChar(s):
+        charCount = {}
+        s = s.lower()
+        for char in s:
+            if char in charCount:
+                charCount[char] += 1
+            else:
+                charCount[char] = 1
+        return charCount
+
+    chars_in_book = numOfChar(file_contents)
+    print(chars_in_book)
 
 
 if __name__ == '__main__':
